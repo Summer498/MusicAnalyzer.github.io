f0bd5eabd621d7b4a8d2dcc052efc2e8999dd26c
Summer498 <h262sawamura@i.softbank.jp>
2022-08-04 19:05:41 +0900

    Add some HTML & SVG element generator

--- lib/HTML.ts
+++ lib/HTML.ts
@@ -1,4 +1,5 @@
 import { Arraying } from "./stdlib.js"
+import { recurrentArray } from "./stdlib.js"
 
 type attribute = { [key: string]: string }
 
@@ -6,7 +7,7 @@ declare global {
 	interface Element {
 		set: (attributes: attribute) => Element
 		setText: (text: string) => Element
-		append: (elements: Element | Element[]) => Element
+		append: (elements: recurrentArray<Element>) => Element
 	}
 }
 
@@ -23,15 +24,55 @@ Element.prototype.append = function (elements) {
 	return this;
 }
 
-export function htmlElement<T extends keyof HTMLElementTagNameMap>(tag: T, attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) {
-	const e = document.createElement<T>(tag)
-	for (const k in attributes) { e.setAttribute(k, attributes[k]) }
-	e.appendChild<Text>(document.createTextNode(text))
-	Arraying(children).forEach(child => e.appendChild(child))
-	return e
+function setComponentsToElement<T extends Element>(
+	element: T,
+	attributes: attribute = {},
+	text: string = "",
+	children: recurrentArray<Element> = []
+) {
+	for (const key in attributes) { element.setAttribute(key, attributes[key]) }
+	element.appendChild<Text>(document.createTextNode(text))
+	Arraying(children).forEach(child => element.appendChild(child))
+	return element
 }
 
-export default class {
+function htmlElement<T extends keyof HTMLElementTagNameMap>(
+	tag: T,
+	attributes: attribute = {},
+	text: string = "",
+	children: recurrentArray<Element> = []
+) {
+	return setComponentsToElement(
+		document.createElement<T>(tag),
+		attributes,
+		text,
+		children
+	)
+}
+
+function svgElement<T extends keyof SVGElementTagNameMap>(
+	qualifiedName: T,
+	attributes: attribute = {},
+	text: string = "",
+	children: recurrentArray<Element> = []
+) {
+	return setComponentsToElement(
+		document.createElementNS("http://www.w3.org/2000/svg", qualifiedName),
+		attributes,
+		text,
+		children
+	)
+}
+
+export class SVG{
+	static svg(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return svgElement("svg", attributes, text, children) }
+	static g(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return svgElement("g", attributes, text, children) }
+	static rect(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return svgElement("rect", attributes, text, children) }
+	static line(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return svgElement("line", attributes, text, children) }
+	static text(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return svgElement("text", attributes, text, children) }
+}
+
+export class HTML {
 	// getters
 	//get  () { return document.getElementsByTagName("") }
 	static get base() { return document.getElementsByTagName("base")[0] }
@@ -40,16 +81,16 @@ export default class {
 	static get title() { return document.getElementsByTagName("title")[0] }
 
 	// creators
-	//(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("", attributes, text, children) }
-	static div(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("div", attributes, text, children) }
-	static span(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("span", attributes, text, children) }
-	static p(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("p", attributes, text, children) }
-	static h1(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("h1", attributes, text, children) }
-	static h2(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("h2", attributes, text, children) }
-	static h3(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("h3", attributes, text, children) }
-	static h4(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("h4", attributes, text, children) }
-	static h5(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("h5", attributes, text, children) }
-	static h6(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("h6", attributes, text, children) }
-	static ol(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("ol", attributes, text, children) }
-	static li(attributes: attribute, text: string = "", children: HTMLElement | HTMLElement[] = []) { return htmlElement("li", attributes, text, children) }
+	//(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("", attributes, text, children) }
+	static div(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("div", attributes, text, children) }
+	static span(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("span", attributes, text, children) }
+	static p(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("p", attributes, text, children) }
+	static h1(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("h1", attributes, text, children) }
+	static h2(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("h2", attributes, text, children) }
+	static h3(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("h3", attributes, text, children) }
+	static h4(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("h4", attributes, text, children) }
+	static h5(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("h5", attributes, text, children) }
+	static h6(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("h6", attributes, text, children) }
+	static ol(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("ol", attributes, text, children) }
+	static li(attributes: attribute={}, text: string = "", children: recurrentArray<Element> = []) { return htmlElement("li", attributes, text, children) }
 }
