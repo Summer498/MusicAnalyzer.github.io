3513f66c2b07791429fbe65c2a4b1f451e486a74
Summer498 <h262sawamura@i.softbank.jp>
2022-08-10 13:32:37 +0900

    add note drawing

--- music-analyzer.js
+++ music-analyzer.js
@@ -2,12 +2,14 @@ import * as Math from "./lib/math.js";
 import { HTML } from "./lib/HTML.js";
 import { SVG } from "./lib/HTML.js";
 
+//TODO: もっとスマートに書く
 function print(msg) {
 	let _printable = null;
 	let printable = _printable ??= document.querySelector(".print");
 	printable.innerHTML = "<span class=\"print\">" + msg + "</span>";
 }
 
+// TODO: Obsolete:
 const chordParse = chord_string => {
 	const str = chord_string;
 	const root = (str[1] == "b" || str[1] == "#") ? str.slice(0, 2) : str[0];
@@ -19,18 +21,37 @@ const chordParse = chord_string => {
 	};
 };
 
-const is_chord_quality_legal = (chord) => {
+// TODO: Obsolete:
+const isChordQualityLegal = (chord) => {
 	const legal_qualities = ["", "6", "7", "M7", "m7", "m", "sus2", "sus4", "add9", "aug"];
-	if (!legal_qualities.includes(chord.quality)) { console.error('illegal chord quality "' + chord.quality + '" received'); }
+	if (!legal_qualities.includes(chord.quality)) { throw Error('illegal chord quality "' + chord.quality + '" received'); }
 };
 
-const body = HTML.body;
+const getChordTone = (chord_string) => {
+	if (chord_string == "N") { return []; }
+	const before_slash = chord_string.indexOf("/");
+	const body_length = (before_slash >= 0) ? before_slash : chord_string.length;
+	const body = chord_string.slice(0, body_length);
+	const base = chord_string.slice(body_length + 1);
+	const notes = Tonal.Chord.get(body).notes;
+	if (notes.length == 0) { throw Error('illegal chord symbol "' + chord_string + '" received'); }
+	if (base == '' || notes.includes(base)) { return notes; }
+	notes.push(base);
+	return notes;
+};
 
+
+
+
+
+
+/* HTML の描画 */
+const body = HTML.body;
 const octave_height = 75,
-	whitekey_width = 40,
-	whitekey_height = octave_height / 7,
-	blackkey_width = whitekey_width / 2,
-	blackkey_height = octave_height / 12;
+	white_key_width = 40,
+	white_key_height = octave_height / 7,
+	black_key_width = white_key_width / 2,
+	black_key_height = octave_height / 12;
 
 const background =
 	SVG.svg({ x: -150.86931316534526 + "%", width: 649.2850333651097 + "%" }, "",
@@ -56,8 +77,8 @@ const piano_roll =
 		Math.Range(3, 7).map(e =>
 			SVG.svg({ y: (6 - e) * 75 }, "",
 				SVG.g({ id: "octave" }, "", [
-					Math.Range(0, 7).map(e2 => SVG.rect({ class: "white-key", x: 0, y: e2 * whitekey_height, width: whitekey_width, height: whitekey_height })),
-					Math.Range(0, 5).map(e2 => SVG.rect({ class: "black-key", x: 0, y: (2 * e2 + (e2 > 2 ? 2 : 1)) * blackkey_height, width: blackkey_width, height: blackkey_height })),
+					Math.Range(0, 7).map(e2 => SVG.rect({ class: "white-key", x: 0, y: e2 * white_key_height, width: white_key_width, height: white_key_height })),
+					Math.Range(0, 5).map(e2 => SVG.rect({ class: "black-key", x: 0, y: (2 * e2 + (e2 > 2 ? 2 : 1)) * black_key_height, width: black_key_width, height: black_key_height })),
 					SVG.text({ class: "label", x: 25, y: 72 }, "C" + String(e))
 				])
 			)
@@ -74,7 +95,24 @@ console.log(melody_timeline);
 //document.insertBefore(white_key, insert_here);
 body.appendChild(melody_timeline);
 
-console.log(Tonal.transpose("A4", "P5"));
+
+
+class moveObject {
+	update() { }
+}
+
+class note extends moveObject {
+	constructor(x, y, width, height) {
+		super();
+		this.rect = SVG.rect({ class: "note", x: x, y: y, width: width, height: height });
+		melody_timeline.appendChild(this.rect);
+	}
+	update() {
+		this.rect.x -= 1;
+		console.log(this.rect.x)
+	}
+}
+
 
 self.onSongleAPIReady = Songle => {
 	const player = new Songle.Player({ mediaElement: "#songle-yt" });
@@ -84,6 +122,9 @@ self.onSongleAPIReady = Songle => {
 	player.addPlugin(new Songle.Plugin.Chorus());
 	player.addPlugin(new Songle.Plugin.SongleWidget({ element: "#songle-sw" }));
 	player.useMedia("https://www.youtube.com/watch?v=zweVJrnE1uY");
+
+
+
 
 	player.on("beatPlay", ev => {
 		const beatElement = document.querySelector(".beat");
@@ -96,16 +137,37 @@ self.onSongleAPIReady = Songle => {
 		beatInfo2Element.textContent = ev.data.beat.numberOfBeatsPerBar;
 	});
 
+
+
+
 	player.on("chordEnter", ev => {
 		const chordNameElement = document.querySelector(".chord .name");
+
+		// 自作関数によるパース
 		chordNameElement.textContent = ev.data.chord.name;
 		const chord = ev.data.chord.name;
 		const parsed = chordParse(chord);
 		console.log(chord, parsed);
-		is_chord_quality_legal(parsed);
-		console.log(Tonal.Chord.get(chord).notes)
+		isChordQualityLegal(parsed);
+
+		// Tonal.js に渡してパース
+		const chord_tone = getChordTone(chord);
+		const chord_tone_number = chord_tone.map(e => Tonal.Midi.toMidi(e + "4"));
+		console.log(chord_tone);
+		console.log(chord_tone_number);
+		chord_tone_number.forEach(e => {
+			melody_timeline.appendChild(
+				SVG.rect({ class: "note", x: 0, y: black_key_height * (e - 36), width: 100, height: black_key_height })
+			);
+			movableObjectsQueue.push(new note(500, black_key_height * (e - 36), 100, black_key_height));
+		});
+		console.log(movableObjectsQueue)
 		// TODO: on コードの構成音を取得できるようにする
+
 	});
+
+
+
 
 	// 盛り上がり部分に入った
 	player.on("repeatSectionEnter", ev => {
@@ -118,6 +180,9 @@ self.onSongleAPIReady = Songle => {
 		document.querySelectorAll("[class*=songle-api-plugin] a").forEach(e => { e.style.color = "#eee"; });
 	});
 
+
+
+
 	player.on("repeatSectionLeave", ev => {
 		const id = ev.data.section.index;
 		if (id != 0 && id != 2) { return; }
@@ -128,3 +193,7 @@ self.onSongleAPIReady = Songle => {
 		document.querySelectorAll("[class*=songle-api-plugin] a").forEach(e => { e.style.color = "#e17"; });
 	});
 };
+
+
+const movableObjectsQueue = [];
+movableObjectsQueue.forEach(e => e.update());
