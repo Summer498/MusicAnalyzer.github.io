4a45daf4203a680ad7c557d3a49dd94ca64ce857
Summer498 <h262sawamura@i.softbank.jp>
2022-08-30 14:41:54 +0900

    change to use ChordObjectClass

--- lib/TonalEx.ts
+++ lib/TonalEx.ts
@@ -1,13 +1,29 @@
 declare let Tonal: any;
 
-type ChordObj = {
-    tonic: string;
-    notes: string[];
-    root: string;
-    symbol: string;
+export class ChordObject {
+    readonly _tonic: string;
+    readonly _notes: string[];
+    readonly _root: string;
+    readonly _symbol: string;
+    constructor(
+        tonic: string,
+        notes: string[],
+        root: string,
+        symbol: string
+    ) {
+        this._tonic = tonic;
+        this._notes = notes;
+        this._root = root;
+        this._symbol = symbol;
+    }
+    static get none(){
+        return new ChordObject('', [], '', '');
+    }
+    get tonic() { return this._tonic; }
+    get notes() { return this._notes; }
+    get root() { return this._root; }
+    get symbol() { return this._symbol; }
 }
-
-export const noneChordObj = { tonic: '', notes: [], root: '', symbol: '' };
 
 const getBodyAndRoot = (chord_string: string) => {
     const before_slash = chord_string.indexOf("/");
@@ -18,14 +34,20 @@ const getBodyAndRoot = (chord_string: string) => {
     return { body, root };
 };
 
-export const getChordInfo = (chord_string: string): ChordObj => {
+export const getChordInfo = (chord_string: string): ChordObject => {
     const body_and_root = getBodyAndRoot(chord_string);
     const root = body_and_root.root;
     const chord = Tonal.Chord.get(body_and_root.body);
-    const notes = chord.notes;
-    const tonic = chord.tonic;
-    const chord_object = { tonic, notes, root: root == '' ? tonic : root, symbol: chord_string };
-    if (chord_string === "") { return noneChordObj; }
+    const notes: string[] = chord.notes;
+    const tonic: string = chord.tonic;
+    const chord_object
+        = new ChordObject(
+            tonic,
+            notes,
+            root == '' ? tonic : root,
+            chord_string
+        );
+    if (chord_string === "") { return ChordObject.none; }
     if (chord.empty) { throw Error('illegal chord symbol "' + chord_string + '" received'); }
     if (root == '' || notes.includes(root)) { }
     else { notes.push(root); }
