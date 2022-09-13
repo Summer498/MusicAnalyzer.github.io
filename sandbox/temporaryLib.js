0ad0b2ed86689f764b6b1ec292979537db73e0ef
Summer498 <h262sawamura@i.softbank.jp>
2022-09-13 10:05:34 +0900

    change temporaryLib to TypeScript

--- sandbox/temporaryLib.js
+++ sandbox/temporaryLib.js
@@ -1,56 +1,44 @@
-import { HTML } from "../lib/HTML/HTML.js";
 import * as Math from "../lib/Math/Math.js";
 "use strict";
-
-export function accidental_num(accidental_symbol) {
-    console.assert(typeof accidental_symbol == "string");
+export const accidental_num = (accidental_symbol) => {
     const accidental_index = "b♮#".indexOf(accidental_symbol);
     console.assert(accidental_index >= 0);
     return accidental_index - 1;
-}
-
-export function chroma(note) {
-    console.assert(typeof note == "string");
+};
+export const chroma = (note) => {
     const len = note.length;
     console.assert(len >= 1 && len < 3);
     const char_num = "C1D3EF6G8A0B".indexOf(note[0]);
     console.assert(char_num >= 0);
-
     return len != 2 ? char_num : (char_num + accidental_num(note[1])).mod(12);
-}
-
-export function keySignature(key) {
-    console.assert(typeof key == "string" || isNaN(key) == false);
+};
+export const keySignature = (key) => {
+    const key_num = Number(key);
+    if (isNaN(key_num) == false) {
+        return Math.mod(key_num * 7 + 5, 12) - 5;
+    }
+    if (typeof key == "number") {
+        throw new Error("Unexpected code executed");
+    }
     const len = key.length;
     console.assert(len >= 1 && len < 3);
-    if (isNaN(key) == false) { return (key * 7 + 5).mod(12) - 5; }
-    else {
-        const capital_key = key[0].toUpperCase() + (len >= 2 ? key[1] : "");  // 先頭を大文字に変換
-
-        // minor なら 3 半音下げる
-        const base = "abcdefg".indexOf(key[0]) >= 0 ? -3 : 0;
-        const accidental_cnt = (chroma(capital_key) * 7 + base + 5).mod(12) - 5;
-        const input_accidental = len == 2 ? accidental_num(key[1]) : 0;
-
-        // 調号が 5 つより多い場合, 入力されたキーの調号に合わせて#,bを付ける. e.g.)#5 -> b7
-        if (Math.abs(accidental_cnt) >= 5) {
-            if (input_accidental != Math.sign(accidental_cnt)) { return accidental_cnt + input_accidental * 12; }
+    const capital_key = key[0].toUpperCase() + (len >= 2 ? key[1] : ""); // 先頭を大文字に変換
+    // minor なら 3 半音下げる
+    const base = "abcdefg".indexOf(key[0]) >= 0 ? -3 : 0;
+    const accidental_cnt = (chroma(capital_key) * 7 + base + 5).mod(12) - 5;
+    const input_accidental = len == 2 ? accidental_num(key[1]) : 0;
+    // 調号が 5 つより多い場合, 入力されたキーの調号に合わせて#,bを付ける. e.g.)#5 -> b7
+    if (Math.abs(accidental_cnt) >= 5) {
+        if (input_accidental != Math.sign(accidental_cnt)) {
+            return accidental_cnt + input_accidental * 12;
         }
-        return accidental_cnt;
     }
-}
-
-export function str_key_signature(key_signature) {
-    console.assert(isNaN(key_signature) == false);
-    return "b♮#"[Math.sign(key_signature) + 1] + String(Math.abs(key_signature));
-}
-
+    return accidental_cnt;
+};
+export const str_key_signature = (key_signature) => "b♮#"[Math.sign(key_signature) + 1] + String(Math.abs(key_signature));
 // ノートナンバーとキーからシンボルを得る
-export function note_symbol(note, key_signature = 0) {
-    if (isNaN(note) == true) {throw new TypeError("note must be number");}
-    if (isNaN(key_signature) == true) {throw new TypeError("note must be number");}
+export const note_symbol = (note, key_signature = 0) => {
     const symbol = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"][note.mod(12)];
-
     if (key_signature < 0) {
         const translate = {
             "C#": "Db",
@@ -59,48 +47,49 @@ export function note_symbol(note, key_signature = 0) {
             "G#": "Ab",
             "A#": "Bb"
         };
-        if (symbol in translate) { return translate[symbol]; }
+        if (symbol in translate) {
+            return translate[symbol];
+        }
     }
-
     if (key_signature >= 6) {
-        if (symbol == "F") { return "E#"; }
-        if (symbol == "C") { return "B#"; }
+        if (symbol == "F") {
+            return "E#";
+        }
+        if (symbol == "C") {
+            return "B#";
+        }
     }
     if (key_signature <= -6) {
-        if (symbol == "E") { return "Fb"; }
-        if (symbol == "B") { return "Cb"; }
+        if (symbol == "E") {
+            return "Fb";
+        }
+        if (symbol == "B") {
+            return "Cb";
+        }
     }
-
     return symbol;
-}
-
+};
 // シンボルから音程を取る
-export function interval(note_symbol0, note_symbol1) {
-    console.assert(typeof note_symbol0 == "string");
-    console.assert(typeof note_symbol1 == "string");
+export const interval = (note_symbol0, note_symbol1) => {
     const deg = [
         "ABCDEFG".indexOf(note_symbol0[0]),
         "ABCDEFG".indexOf(note_symbol1[0])
     ];
     console.assert(deg[0] >= 0 && deg[1] >= 0);
-
     const deg_diff = (deg[1] - deg[0]).mod(7);
     const acc_symbol = (chroma(note_symbol1) - chroma(note_symbol0) - [0, 1, 3, 5, 7, 8, 10][deg_diff] + 6).mod(12) - 6;
-
-    if (deg_diff == 0 && acc_symbol < 0) { return [acc_symbol, 8]; }
+    if (deg_diff == 0 && acc_symbol < 0) {
+        return [acc_symbol, 8];
+    }
     return [acc_symbol, deg_diff + 1];
-}
-
-export function inv_interval(interval) {
-    console.assert(typeof interval == "object");
-    console.assert(isNaN(interval[0]) == false);
-    console.assert(isNaN(interval[1]) == false);
-    if (interval[1] in [2, 3, 6, 7]) { return [1 - interval[0], 9 - interval[1]]; }
+};
+export const inv_interval = (interval) => {
+    if (interval[1] in [2, 3, 6, 7]) {
+        return [1 - interval[0], 9 - interval[1]];
+    }
     return [-interval[0], 9 - interval[1]];
-}
-
-export function str_interval(interval) {
-    console.assert(isNaN(interval[0]) == false);
-    const table = [1, 4, 5, 8].indexOf(interval[1]) >= 0 ?["ddd", "dd", "d", "P", "A", "AA", "AAA"] :["ddd", "dd", "d", "m", "M", "A", "AA", "AAA"];
+};
+export const str_interval = (interval) => {
+    const table = [1, 4, 5, 8].indexOf(interval[1]) >= 0 ? ["ddd", "dd", "d", "P", "A", "AA", "AAA"] : ["ddd", "dd", "d", "m", "M", "A", "AA", "AAA"];
     return table[interval[0] + 3] + String(interval[1]);
-}
+};
---
+++ sandbox/temporaryLib.ts
@@ -0,0 +1,94 @@
+import * as Math from "../lib/Math/Math.js";
+"use strict";
+
+export const accidental_num = (accidental_symbol: string) => {
+    const accidental_index = "b♮#".indexOf(accidental_symbol);
+    console.assert(accidental_index >= 0);
+    return accidental_index - 1;
+};
+
+export const chroma = (note: string) => {
+    const len = note.length;
+    console.assert(len >= 1 && len < 3);
+    const char_num = "C1D3EF6G8A0B".indexOf(note[0]);
+    console.assert(char_num >= 0);
+
+    return len != 2 ? char_num : (char_num + accidental_num(note[1])).mod(12);
+};
+
+export const keySignature = (key: string | number) => {
+    const key_num = Number(key);
+    if (isNaN(key_num) == false) { return Math.mod(key_num * 7 + 5, 12) - 5; }
+    if (typeof key == "number") { throw new Error("Unexpected code executed"); }
+
+    const len = key.length;
+    console.assert(len >= 1 && len < 3);
+    const capital_key = key[0].toUpperCase() + (len >= 2 ? key[1] : "");  // 先頭を大文字に変換
+
+    // minor なら 3 半音下げる
+    const base = "abcdefg".indexOf(key[0]) >= 0 ? -3 : 0;
+    const accidental_cnt = (chroma(capital_key) * 7 + base + 5).mod(12) - 5;
+    const input_accidental = len == 2 ? accidental_num(key[1]) : 0;
+
+    // 調号が 5 つより多い場合, 入力されたキーの調号に合わせて#,bを付ける. e.g.)#5 -> b7
+    if (Math.abs(accidental_cnt) >= 5) {
+        if (input_accidental != Math.sign(accidental_cnt)) { return accidental_cnt + input_accidental * 12; }
+    }
+    return accidental_cnt;
+};
+
+export const str_key_signature = (key_signature: number) =>
+    "b♮#"[Math.sign(key_signature) + 1] + String(Math.abs(key_signature));
+
+// ノートナンバーとキーからシンボルを得る
+export const note_symbol = (note: number, key_signature = 0) => {
+    const symbol = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"][note.mod(12)];
+
+    if (key_signature < 0) {
+        const translate: { [key: string]: string }
+            = {
+            "C#": "Db",
+            "D#": "Eb",
+            "F#": "Gb",
+            "G#": "Ab",
+            "A#": "Bb"
+        };
+        if (symbol in translate) { return translate[symbol]; }
+    }
+
+    if (key_signature >= 6) {
+        if (symbol == "F") { return "E#"; }
+        if (symbol == "C") { return "B#"; }
+    }
+    if (key_signature <= -6) {
+        if (symbol == "E") { return "Fb"; }
+        if (symbol == "B") { return "Cb"; }
+    }
+
+    return symbol;
+};
+
+// シンボルから音程を取る
+export const interval = (note_symbol0: string, note_symbol1: string) => {
+    const deg = [
+        "ABCDEFG".indexOf(note_symbol0[0]),
+        "ABCDEFG".indexOf(note_symbol1[0])
+    ];
+    console.assert(deg[0] >= 0 && deg[1] >= 0);
+
+    const deg_diff = (deg[1] - deg[0]).mod(7);
+    const acc_symbol = (chroma(note_symbol1) - chroma(note_symbol0) - [0, 1, 3, 5, 7, 8, 10][deg_diff] + 6).mod(12) - 6;
+
+    if (deg_diff == 0 && acc_symbol < 0) { return [acc_symbol, 8]; }
+    return [acc_symbol, deg_diff + 1];
+};
+
+export const inv_interval = (interval: number[]) => {
+    if (interval[1] in [2, 3, 6, 7]) { return [1 - interval[0], 9 - interval[1]]; }
+    return [-interval[0], 9 - interval[1]];
+};
+
+export const str_interval = (interval: number[]) => {
+    const table = [1, 4, 5, 8].indexOf(interval[1]) >= 0 ? ["ddd", "dd", "d", "P", "A", "AA", "AAA"] : ["ddd", "dd", "d", "m", "M", "A", "AA", "AAA"];
+    return table[interval[0] + 3] + String(interval[1]);
+};
