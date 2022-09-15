ee823a1892a178b39744d0c552b03c53b6f08dbc
Summer498 <h262sawamura@i.softbank.jp>
2022-09-15 09:39:33 +0900

    deprecate all temporary lib functions

--- sandbox/temporaryLib.js
+++ sandbox/temporaryLib.js
@@ -1,17 +1,21 @@
 import * as Math from "../lib/Math/Math.js";
 "use strict";
+/* eslint-disable deprecation/deprecation */
+/** @deprecated */
 export const accidental_num = (accidental_symbol) => {
     const accidental_index = "b♮#".indexOf(accidental_symbol);
     console.assert(accidental_index >= 0);
     return accidental_index - 1;
 };
+/** @deprecated */
 export const chroma = (note) => {
     const len = note.length;
     console.assert(len >= 1 && len < 3);
     const char_num = "C1D3EF6G8A0B".indexOf(note[0]);
     console.assert(char_num >= 0);
-    return len != 2 ? char_num : (char_num + accidental_num(note[1])).mod(12);
+    return len != 2 ? char_num : Math.mod(char_num + accidental_num(note[1]), 12);
 };
+/** @deprecated */
 export const keySignature = (key) => {
     const key_num = Number(key);
     if (isNaN(key_num) == false) {
@@ -35,10 +39,12 @@ export const keySignature = (key) => {
     }
     return accidental_cnt;
 };
+/** @deprecated */
 export const str_key_signature = (key_signature) => "b♮#"[Math.sign(key_signature) + 1] + String(Math.abs(key_signature));
 // ノートナンバーとキーからシンボルを得る
+/** @deprecated */
 export const note_symbol = (note, key_signature = 0) => {
-    const symbol = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"][note.mod(12)];
+    const symbol = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"][Math.mod(note, 12)];
     if (key_signature < 0) {
         const translate = {
             "C#": "Db",
@@ -70,25 +76,28 @@ export const note_symbol = (note, key_signature = 0) => {
     return symbol;
 };
 // シンボルから音程を取る
+/** @deprecated */
 export const interval = (note_symbol0, note_symbol1) => {
     const deg = [
         "ABCDEFG".indexOf(note_symbol0[0]),
         "ABCDEFG".indexOf(note_symbol1[0])
     ];
     console.assert(deg[0] >= 0 && deg[1] >= 0);
-    const deg_diff = (deg[1] - deg[0]).mod(7);
-    const acc_symbol = (chroma(note_symbol1) - chroma(note_symbol0) - [0, 1, 3, 5, 7, 8, 10][deg_diff] + 6).mod(12) - 6;
+    const deg_diff = Math.mod(deg[1] - deg[0], 7);
+    const acc_symbol = Math.mod(chroma(note_symbol1) - chroma(note_symbol0) - [0, 1, 3, 5, 7, 8, 10][deg_diff] + 6, 12) - 6;
     if (deg_diff == 0 && acc_symbol < 0) {
         return [acc_symbol, 8];
     }
     return [acc_symbol, deg_diff + 1];
 };
+/** @deprecated */
 export const inv_interval = (interval) => {
     if (interval[1] in [2, 3, 6, 7]) {
         return [1 - interval[0], 9 - interval[1]];
     }
     return [-interval[0], 9 - interval[1]];
 };
+/** @deprecated */
 export const str_interval = (interval) => {
     const table = [1, 4, 5, 8].indexOf(interval[1]) >= 0 ? ["ddd", "dd", "d", "P", "A", "AA", "AAA"] : ["ddd", "dd", "d", "m", "M", "A", "AA", "AAA"];
     return table[interval[0] + 3] + String(interval[1]);
--- sandbox/temporaryLib.ts
+++ sandbox/temporaryLib.ts
@@ -1,21 +1,25 @@
 import * as Math from "../lib/Math/Math.js";
 "use strict";
 
+/* eslint-disable deprecation/deprecation */
+/** @deprecated */
 export const accidental_num = (accidental_symbol: string) => {
     const accidental_index = "b♮#".indexOf(accidental_symbol);
     console.assert(accidental_index >= 0);
     return accidental_index - 1;
 };
 
+/** @deprecated */
 export const chroma = (note: string) => {
     const len = note.length;
     console.assert(len >= 1 && len < 3);
     const char_num = "C1D3EF6G8A0B".indexOf(note[0]);
     console.assert(char_num >= 0);
 
-    return len != 2 ? char_num : (char_num + accidental_num(note[1])).mod(12);
+    return len != 2 ? char_num : Math.mod(char_num + accidental_num(note[1]), 12);
 };
 
+/** @deprecated */
 export const keySignature = (key: string | number) => {
     const key_num = Number(key);
     if (isNaN(key_num) == false) { return Math.mod(key_num * 7 + 5, 12) - 5; }
@@ -37,12 +41,14 @@ export const keySignature = (key: string | number) => {
     return accidental_cnt;
 };
 
+/** @deprecated */
 export const str_key_signature = (key_signature: number) =>
     "b♮#"[Math.sign(key_signature) + 1] + String(Math.abs(key_signature));
 
 // ノートナンバーとキーからシンボルを得る
+/** @deprecated */
 export const note_symbol = (note: number, key_signature = 0) => {
-    const symbol = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"][note.mod(12)];
+    const symbol = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"][Math.mod(note, 12)];
 
     if (key_signature < 0) {
         const translate: { [key: string]: string }
@@ -69,6 +75,7 @@ export const note_symbol = (note: number, key_signature = 0) => {
 };
 
 // シンボルから音程を取る
+/** @deprecated */
 export const interval = (note_symbol0: string, note_symbol1: string) => {
     const deg = [
         "ABCDEFG".indexOf(note_symbol0[0]),
@@ -76,18 +83,20 @@ export const interval = (note_symbol0: string, note_symbol1: string) => {
     ];
     console.assert(deg[0] >= 0 && deg[1] >= 0);
 
-    const deg_diff = (deg[1] - deg[0]).mod(7);
-    const acc_symbol = (chroma(note_symbol1) - chroma(note_symbol0) - [0, 1, 3, 5, 7, 8, 10][deg_diff] + 6).mod(12) - 6;
+    const deg_diff = Math.mod(deg[1] - deg[0], 7);
+    const acc_symbol = Math.mod(chroma(note_symbol1) - chroma(note_symbol0) - [0, 1, 3, 5, 7, 8, 10][deg_diff] + 6, 12) - 6;
 
     if (deg_diff == 0 && acc_symbol < 0) { return [acc_symbol, 8]; }
     return [acc_symbol, deg_diff + 1];
 };
 
+/** @deprecated */
 export const inv_interval = (interval: number[]) => {
     if (interval[1] in [2, 3, 6, 7]) { return [1 - interval[0], 9 - interval[1]]; }
     return [-interval[0], 9 - interval[1]];
 };
 
+/** @deprecated */
 export const str_interval = (interval: number[]) => {
     const table = [1, 4, 5, 8].indexOf(interval[1]) >= 0 ? ["ddd", "dd", "d", "P", "A", "AA", "AAA"] : ["ddd", "dd", "d", "m", "M", "A", "AA", "AAA"];
     return table[interval[0] + 3] + String(interval[1]);
