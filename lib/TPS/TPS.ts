f1d30a75eea12811df83fd9b7235a299a98bf371
Summer498 <h262sawamura@i.softbank.jp>
2022-09-18 09:50:45 +0900

    delete noisy log

--- lib/TPS/TPS.ts
+++ lib/TPS/TPS.ts
@@ -83,13 +83,9 @@ export const basicSpaceDistance = (src: RomanChord, dst: RomanChord) => {
 export const getDistance = (src_chord_string: RomanChord, dst_chord_string: RomanChord): number => {
 	const src = src_chord_string;
 	const dst = dst_chord_string;
-	console.log(src, dst);
 	const region_dist = regionDistance(src.scale, dst.scale);
-	console.log("region_dist", region_dist);
 	const tonic_dist = tonicDistance(src.chord, dst.chord);
-	console.log("root_dist", tonic_dist);
 	const basic_space_dist = basicSpaceDistance(src, dst);
-	console.log("basic_space_dist", basic_space_dist);
 	return region_dist + tonic_dist + basic_space_dist; //dummy
 };
 
